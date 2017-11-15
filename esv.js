abaa31b5704cb1d4791e863df13b75a24d8e2c06
akim <akim@mvixusa.com>
2017-11-15 16:41:29 -0500

    first push of esv project

---
+++ esv.js
@@ -0,0 +1,150 @@
+//curl -X GET --header 'Accept: application/json' --header 'Authorization: Token a23824c69af78a425dd3fbcef39eaf61e2ffd834'    
+//Mark 77 1-11 ???
+var verses= ['Luke2%3A40-52','Matthew3%3A1-6', 'Matthew13%3A17', 'Mark1%3A10-11', 'Luke3%3A22-23', 'Matthew4%3A1-11', 'Mark1%3A13', 'Luke4%3A1-13', 'John1%3A19-20', 'John1%3A22-29', 'John1%3A37-51', 'John2%3A1-9', 'John2%3A12-16', 'John2%3A18-21', 'John3%3A1-21', 'John3%3A22', 'John28%3A1-40', 'John4%3A43-53', 'John5%3A1-47', 'Luke4%3A16-31', 'Mark1%3A15', 'Matthew4%3A17', 'Mark1%3A16-26', 'Luke4%3A31', 'Luke4%3A38', 'Luke4%3A44', 'Matthew4%3A17', 'Matthew4%3A25', 'Matthew5%3A1-48', 'Matthew6%3A1-34', 'Matthew7%3A1-29', 'Matthew8%3A1-4', 'Matthew9%3A2-7', 'Mark1%3A40-45', 'Mark2%3A1-12', 'Luke5%3A1-15', 'Luke5%3A18-25', 'Luke5%3A27-39', 'Matthew9%3A9-17', 'Mark2%3A13-22', 'Matthew12%3A1-16', 'Mark2%3A23-38', 'Mark3%3A1-15', 'Mark3%3A22-29', 'Luke6%3A1-13', 'Luke11%3A24-26', 'Matthew10%3A2-4', 'Mark3%3A16-19', 'Luke6%3A14-38', 'Luke6%3A39-49', 'Matthew8%3A5-13', 'Luke7%3A11-18', 'Matthew11%3A2-15', 'Matthew11%3A16-19', 'Luke7%3A24-35', 'Matthew11%3A20-30', 'Luke10%3A13-15', 'Luke7%3A36-50', 'Matthew12%3A22-37', 'Luke11%3A17-23', 'Mark3%3A22-39', 'Matthew12%3A38-45', 'Matthew12%3A46-50', 'Matthew13%3A1-30', 'Mark3%3A31-35', 'Mark4%3A1-29', 'Luke8%3A4-18', 'Luke8%3A19-21', 'Mark4%3A30-34', 'Mark4%3A35-41', 'Matthew13%3A31-34', 'Matthew13%3A36-52', 'Luke13%3A18-21', 'Luke8%3A22-25', 'Mark5%3A1-20', 'Luke8%3A26-40', 'Matthew8%3A28-34', 'Luke9%3A41-42', 'Luke9%3A49-56', 'Luke9%3A43-48', 'Mark5%3A22-24', 'Mark5%3A35-43', 'Mark5%3A25-34', 'Matthew9%3A18-19', 'Matthew9%3A23-26', 'Matthew9%3A27-30', 'Mark6%3A1-6', 'Mark6%3A7-13', 'Matthew13%3A54-58', 'Matthew9%3A35-38', 'Matthew10%3A5-42', 'Luke9%3A1-6', 'Matthew14%3A1-4', 'Matthew14%3A13-14', 'Luke9%3A7-11', 'Mark6%3A17-18', 'Mark6%3A30-33', 'John6%3A1-3', 'John6%3A5-13', 'Matthew14%3A15-21', 'Mark6%3A35-44', 'Luke9%3A12-17', 'Matthew14%3A22-32', 'Mark6%3A45-53', 'John6%3A15-21', 'John6%3A22-71', 'Mark7%3A1-23', 'Matthew15%3A1-20', 'Matthew15%3A21-28', 'Mark7%3A24-36', 'Mark8%3A1-26', 'Matthew15%3A32-39', 'John6%3A1-12', 'Matthew16%3A13-28', 'Mark8%3A27-38', 'Mark9%3A1', 'Luke9%3A18-27', 'Luke9%3A28-42', 'Matthew17%3A1-21', 'Mark9%3A2-29', 'Mark9%3A30-37', 'Luke9%3A44-48', 'Matthew17%3A22-27', 'Mark9%3A38-50', 'Luke9%3A49-50', 'Matthew18%3A1-35', 'John7%3A1-46', 'John7%3A53', 'John8%3A1', 'John8%3A2-59', 'John9%3A1-41', 'John10%3A1-18', 'John10%3A22-40', 'Luke9%3A51-62', 'Luke10%3A1-24', 'Luke10%3A25-37', 'Luke11%3A1-13', 'Luke11%3A27-54', 'Luke12%3A1-40', 'Luke12%3A41-59', 'Luke13%3A1-9', 'Luke13%3A10-21', 'Luke13%3A22-35', 'Luke10%3A38-42', 'John10%3A39-40', 'Luke14%3A1-35', 'Luke15%3A1-32', 'Luke16%3A1-31', 'Luke17%3A1-10', 'John11%3A1-54', 'Luke17%3A11-37', 'Luke18%3A1-8', 'Luke18%3A9-30', 'Mark10%3A13-31', 'Matthew19%3A13-30', 'Matthew20%3A1-16', 'Mark10%3A1-12', 'Matthew19%3A1-12', 'Matthew20%3A17-28', 'Mark10%3A32-45', 'Luke18%3A31-34', 'Luke18%3A35-43', 'Luke19%3A1-27', 'Mark10%3A46-52', 'John11%3A55-57', 'Matthew21%3A1-6', 'Mark77%3A1-11', 'Luke19%3A28-44','Mark11%3A12-26', 'Matthew21%3A12-13', 'Matthew21%3A18-22', 'Luke19%3A45-48', 'Matthew21%3A23-46', 'Mark11%3A27-33', 'Matthew12%3A1-12', 'Luke20%3A1-19', 'Matthew22%3A1-32', 'Mark12%3A13-27', 'Luke20%3A20-38', 'Matthew22%3A34-46', 'Mark12%3A28-44', 'Luke20%3A41-47', 'Luke21%3A1-4', 'Matthew23%3A1-39', 'John12%3A20-36', 'John12%3A42-50', 'Matthew24%3A1-31', 'Mark13%3A1-27', 'Luke21%3A5-28', 'Matthew24%3A32-51', 'Matthew25%3A1-13', 'Mark13%3A28-37', 'Luke21%3A29-36', 'Matthew25%3A14-46', 'John12%3A1-11', 'Matthew26%3A6-13', 'Mark14%3A3-9', 'Luke21%3A37-38', 'Luke22%3A1-18', 'Matthew26%3A1-5', 'Matthew26%3A14-20', 'Mark14%3A1-2', 'Mark14%3A10-17', 'John13%3A2-17', 'Corinthians11%3A24-25', 'Matthew26%3A21-29', 'Mark14%3A18-25', 'Luke22%3A19-38', 'John13%3A18-38', 'John14%3A1-4', 'John14%3A5-31', 'John15%3A1-27', 'John16%3A1', 'John16%3A2-33', 'John17%3A1-26', 'Matthew26%3A30-47', 'Mark14%3A26-43', 'Luke22%3A39-47', 'John18%3A1-9', 'Matthew26%3A48-58', 'Mark14%3A44-50', 'Mark14%3A53-54', 'Luke22%3A47-55', 'John18%3A10-16', 'John18%3A19-23', 'Matthew26%3A59-75', 'Matthew27%3A1-7', 'Mark14%3A55-72', 'Luke22%3A56-71', 'John18%3A17-27', 'John18%3A28-40', 'John19%3A1-16', 'Luke23%3A1-11', 'Luke23%3A13-25', 'Mark15%3A1-15', 'Matthew27%3A11-26', 'Matthew27%3A27-38', 'Mark15%3A16-27', 'Luke23%3A26-34', 'John19%3A17-20', 'Matthew27%3A39-56', 'Mark15%3A29-41', 'Luke23%3A35-49', 'John19%3A23-30', 'Mark15%3A42-47', 'Luke23%3A50-56', 'John19%3A38-42', 'Matthew27%3A57-66', 'Matthew28%3A1', 'Matthew28%3A8-10', 'Mark16%3A1-11', 'Luke24%3A1-9', 'John20%3A11-18', 'Mark16%3A12-13', 'Luke24%3A13-31', 'Luke24%3A33-43', 'John20%3A19-29', 'John21%3A1-25', 'Matthew28%3A16-20', 'Mark16%3A12-18', 'Luke24%3A44-53', 'Acts1%3A12', 'Acts1%3A2-9', 'Acts8%3A3', 'Acts9%3A1-20', 'Acts10%3A44-45', 'Acts11%3A16', 'Acts18%3A7-11', 'Acts20%3A17-19', 'Acts20%3A22', 'Acts20%3A25', 'Acts20%3A32-38', 'Acts26%3A1-32', 'Corinthians12%3A7-9', 'Revelation1%3A1-3', 'Revelation1%3A9-20', 'Revelation2%3A1-7', 'Revelation2%3A8-11', 'Revelation2%3A12-17', 'Revelation2%3A18-29', 'Revelation3%3A1-6', 'Revelation3%3A7-13', 'Revelation3%3A14-22', 'Revelation4%3A1-2', 'Revelation14%3A13', 'Revelation16%3A15', 'Revelation19%3A9-10', 'Revelation21%3A5-8', 'Revelation22%3A6-21']
+
+//var urls = verses.map(verse => 'https://api.esv.org/v3/passage/text/?q=' + verse);
+var urls= 'https://api.esv.org/v3/passage/text/?q=' + verses.join();
+//verses.forEach(v => {
+//    fetch(myRequest)
+//    .then(resp => {
+//        return resp.json();
+//    }).then(json => {
+//        console.log(json);
+//    });
+//    
+//})  
+
+var myHeaders = new Headers({
+    "Accept": "application/json",
+    "Authorization": "Token a23824c69af78a425dd3fbcef39eaf61e2ffd834"
+});
+
+var myInit= {
+    method: 'GET',
+    headers: myHeaders,
+    mode: 'cors',
+    cache: 'default'
+};
+//var request= 'https://api.esv.org/v3/passage/text/?q=' + v
+//var request= 'https://api.esv.org/v3/passage/text/?q=Luke2%3A40-52'
+
+var myRequest = new Request(urls, myInit);
+
+fetch(myRequest)
+    .then(resp => {
+        return resp.json();
+    }).then(json => {
+        console.log(json);
+    });
+    
+//var promises = urls.map(url => fetch(myRequest).then(y=> y.text()));
+//Promise.all(promises).then(results =>{
+//    console.log(results);
+//})
+      
+ 
+//Matthew 3, 1-6; 13-17: Mark 1, 10-11; Luke 3, 22-23
+//Matthew 4, 1-11: Mark 1, 13: Luke 4, 1-13
+//John, 1, 19-20 . . . 22-29 . . . 37-51
+//John 2, 1-9; 12-16; 18-21.
+//John 3, 1-21.
+//John 3, 22 . . . 28; 1-40.      
+//John 4, 43-53.
+//John 5, 1-47.
+//Luke 4, 16-31; Mark 1, 15; Matthew 4, 17.
+//Mark 1, 16-26; Luke 4, 31 . . . 38 . . . 44; Matthew 4, 17 . . . 25.
+//Matthew 5, 1-48.
+//Matthew 6, 1-34.
+//Matthew 7, 1-29.//      
+//Matthew 8, 1-4; 9, 2-7; Mark 1, 40-45; 2, 1-12; Luke 5, 1-15; and 18-25.//      
+//Luke 5, 27-39; Matthew 9, 9-17; Mark 2, 13-22.//      
+//Matthew 12, 1-16: Mark 2, 23-28; 3, 1-15; 22-29: Luke 6, 1-13; 11, 24-26.//      
+//Matthew 10, 2-4: Mark 3, 16-19: Luke 6, 14-38.//      
+//Luke 6, 39-49.
+//Matthew 8, 5-13: Luke 7, 11-18//      .
+//Matthew 11, 2-15; 16-19: Luke 7, 24-35.//      
+//Matthew 11, 20-30: Luke 10, 13-15.
+//Luke 7, 36-50.
+//Matthew 12, 22-37: Luke 11, 17-23: Mark 3, 22-29.//      
+//Matthew 12, 38-45.
+//Matt. 12, 46-50; 13, 1-30; Mark 3, 31-35; 4, 1-29: Luke 8, 4-18; 19-21.
+//Mk. 4, 30-34; 35-41: Matt. 13, 31-34; 36-52: Lk. 13, 18-21; 8, 22-25.//      
+//Mark 5, 1-20: Luke 8, 26-40: Matthew 8, 28-34.
+//Luke 9, 41-42; 49-56; 43-48: Mark 5, 22-24; 35-43; 25-34: Matthew 9, 18-19; 23-26; 27-30.//      
+//Mark 6, 1-6, 7-13: Matthew 13, 54-58; 9, 35-38; 10, 5-42: Luke 9, 1-6.//      
+//Matthew 14, 1-4; 13-14: Luke 9, 7-11: Mark 6, 17-18; 30-33: John 6, 1-3.      
+//John 6, 5-13: Matthew 14, 15-21: Mark 6, 35-44: Luke 9, 12-17.//
+//Matthew 14, 22-32: Mark 6, 45-53: John 6, 15-21.//
+//John 6, 22-71.//
+//Mark 7, 1-23: Matthew 15, 1-20.
+//Matthew 15, 21-28: Mark 7, 24-36.//      
+//Mark 8, 1-26: Matthew 15, 32-39; John 6, 1-12.//      
+//Matthew 16, 13-28: Mark 8, 27-38; 9, 1: Luke 9, 18-27. *//      
+//Luke 9, 28-42: Matthew 17, 1-21: Mark 9, 2-29.
+//Mark 9, 30-37: Luke 9, 44-48: Matthew 17, 22-27.//      
+//Mark 9, 38-50: Luke 9, 49-50.//      
+//Matthew 18, 1-35.//
+//John 7, 1-46, 53; 8, 1.
+//John 8, 2-59.
+//John 9, 1-41; 10, 1-18; 22-40.
+//Luke 9, 51-62; 10, 1-24//
+//Luke 10, 25-37.
+//Luke 11, 1-13; 27-54.
+//Luke 12, 1-40.
+//Luke 12, 41-59; 13, 1-9.
+//Luke 13, 10-21.
+//Luke 13, 22-35; 10, 38-42: John 10, 39-40.
+//Luke 14, 1-35.
+//Luke 15, 1-32.
+//Luke 16, 1-31.
+//Luke 17, 1-10: John 11, 1-54.
+//Luke 17, 11-37; 18, 1-8.//
+//Luke 18, 9-30: Mark 10, 13-31: Matthew 19, 13-30; and 20, 1-16.//
+//Mark 10, 1-12: Matthew 19, 1-12.
+//Matthew 20, 17-28: Mark 10, 32-45: Luke 18, 31-34.
+//Luke 18, 35-43; 19, 1-27: Mark 10, 46-52.//
+//John 11, 55-57: Matthew 21, 1-6: Mark 77, 1-11: Luke 19, 28-44.
+//Mark 11, 12-26: Matthew 21, 12-13; 18-22: Luke 19, 45-48.
+//Matthew 21, 23-46: Mark 11, 27-33; 12, 1-12: Luke 20, 1-19.//
+//Matthew 22, 1-32: Mark 12, 13-27: Luke 20, 20-38.//
+//Matthew 22, 34-46: Mark 12, 28-44: Luke 20, 41-47; 21, 1-4.//
+//Matthew 23, 1-39.//
+//John 12, 20-36; 42-50.
+//Matthew 24, 1-31: Mark 13, 1-27: Luke 21, 5-28.//
+//Matthew 24, 32-51; 25, 1-13: Mark 13, 28-37: Luke 21, 29-36.//
+//Matthew 25, 14-46.
+//John 12, 1-11: Matthew 26, 6-13: Mark 14, 3-9.
+//Luke 21, 37-38; 22, 1-18: Matthew 26, 1-5; 14-20: Mark 14, 1-2; 10-17: John 13, 2-17.//
+//Paul I. Corinthians 11, 24-25: Matthew 26, 21-29: Mark 14, 18-25: Luke 22, 19-38: John 13, 18-38; 14, 1-4.
+//John 14, 5-31.
+//John 15, 1-27; 16, 1.
+//John 16, 2-33.
+//John 17, 1-26.
+//Matthew 26, 30-47: Mark 14, 26-43: Luke 22, 39-47: John 18, 1-9.//      
+//Matthew 26, 48-58: Mark 14, 44-50; 53-54: Luke 22, 47-55: John 18, 10-16; 19-23.//      
+//Matthew 26, 59-75; 27, 1-7: Mark 14, 55-72: Luke 22, 56-71: John 18, 17-27.//      
+//John 18, 28-40; 19, 1-16: Luke 23, 1-11; 13-25: Mark 15, 1-15: Matthew 27, 11-26.//
+//Matthew 27, 27-38: Mark 15, 16-27: Luke 23, 26-34: John 19, 17-20.//      
+//Matthew 27, 39-56: Mark 15, 29-41: Luke 23, 35-49: John 19, 23-30.//
+//Mark 15, 42-47: Luke 23, 50-56: John 19, 38-42: Matthew 27, 57-66.//      
+//Matthew 28, 1, and 8-10: Mark 16, 1-11: Luke 24, 1-9: John 20, 11-18.
+//Mark 16, 12-13: Luke 24, 13-31; 33-43: John 20, 19-29.//
+//John 21, 1-25.//
+//Matthew 28, 16-20: Mark 16, 12-18: Luke 24, 44-53; and Luke's Acts of The Apostles 1, 12.//
+//Luke: Acts 1, 2-9.
+//Luke: Acts 8, 3; 9, 1-20. Damascus. A.D. 34-35.
+//Luke: Acts 10, 44-45; and 11, 16.
+//Luke: Acts 18, 7-11.
+//Luke: Acts 20, 17-19, 22, 25, 32-38.
+//Luke: Acts (parts of) Chapters 21, 22, 23, 24, 25, 26, 27.
+//Luke: Acts 26, 1-32.
+//Paul: II. Corinthians 12, 7-9.
+//Revelation 1, 1-3; 9-20.
+//Revelation 2, 1-7.
+//Revelation 2, 8-11.
+//Revelation 2, 12-17.//
+//Revelation 2, 18-29.//
+//Revelation 3, 1-6.//
+//Revelation 3, 7-13.
+//Revelation 3, 14-22.
+//Revelation 4, 1-2; 14, 13; 16, 15; 19, 9-10; 21, 5-8; 22, 6-21.
